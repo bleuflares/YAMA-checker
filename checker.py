040e4fc8d408d012a029f9cf0395dbd00a6aa0b3
Roy Park <roy@Roys-MacBook-Air.local>
2020-08-03 01:32:18 +0900

    implemented checking but couldn't implement autocorrection maybe next time...

--- checker.py
+++ checker.py
@@ -39,23 +39,61 @@ def replace_text(content, replacements = dict()):
 def replace_firstpage(content, title_format):
 	lines = content.splitlines()
 	result = ""
+	in_text = False
 	count = 0
 
 	for line in lines:
-		if count == 0:
-			print(line)
-			print(title_format)
-			if line != title_format:
-				print('title format is wrong! automatically converted... please check')
-			line = title_format
-		elif count == 1:
-			question = line.split()
-			question[0] = title_format.split()[1] + '.'
-			if line != ' '.join(question):
-				print('question number format is wrong! automatically converted... please check')
-			line - ' '.join(question)
+		if line == "BT":
+			in_text = True
+
+		elif line == "ET":
+			in_text = False
+		elif in_text:
+			cmd = line[-2:]
+			if cmd.lower() == 'tj':
+				replaced_line = line
+				if count == 0:
+					print(replaced_line)
+					#if line != title_format:
+					#	print('title format is wrong! automatically converted... please check')
+					replaced_line = title_format.decode('utf-8')
+					
+					result += replaced_line + "\n"
+					count += 1
+				elif count == 1:
+					print(replaced_line)
+					question = (line.encode('utf-8')).split()
+					question[0] = title_format.split()[1] + '.'
+					#if line != ' '.join(question):
+					#	print('question number format is wrong! automatically converted... please check')
+					replaced_line = (' '.join(question)).decode('utf-8')
+					count += 1
+					result += replaced_line + "\n"
+				else:
+					result += line + "\n"
+			continue
+
 		result += line + "\n"
 	return result
+
+def check_first_page_noreplace(content, title_format):
+	correct = True
+	text = content.extractText() + '\n'
+	merged_text = (''.join(text.split('\n'))).split(' ')
+	merged_title = merged_text[0] + '차'.decode('utf-8') + ' ' + merged_text[1]
+	merged_q = merged_text[3]
+	#print(merged_title)
+	#print(merged_q)
+	#print(title_format.split())
+	if merged_title.encode('utf-8') != title_format:
+		print('***title format is wrong! please check...***')
+		correct = False
+	if merged_q != title_format.split()[1] + '.':
+		print('***question number format is wrong! please check...***')
+		correct = False
+	if correct:
+		print('correct format... continue!')
+
 
 def process_data(object, replacements):
 	data = object.getData()
@@ -75,7 +113,7 @@ def check_first_page(first_page, title_format):
 	data = contents.getData()
 	decoded_data = data.decode('utf-8')
 	replaced_data = replace_firstpage(decoded_data, title_format)
-	encoded_data = replaced_data.encode('utf-8')
+	encoded_data = replaced_data
 	if contents.decodedSelf is not None:
 		contents.decodedSelf.setData(encoded_data)
 	else:
@@ -89,7 +127,6 @@ if __name__ == "__main__":
 
 	in_file = args["input"].decode('utf-8')
 	uid = args["uid"].decode('utf-8')
-	print(in_file)
 
 	f = open(in_file, 'r')
 	reader = csv.reader(f)
@@ -104,7 +141,6 @@ if __name__ == "__main__":
 		if line_count == 0:
 			subject = line[0].split()[0]
 			line_count += 1
-			print(subject)
 			continue
 
 		if line_count == 1:
@@ -132,24 +168,27 @@ if __name__ == "__main__":
 			filenames.append(subject[3:len(subject)] + ' ' + '_'.join(filename_base) + '.pdf')
 
 	temp_count = 0
-	
 	for filename in filenames:
-		print(filename)
 		if os.path.isfile(filename):
+			print(filename + ' exists checking file...')
 			pdf = PdfFileReader(filename)
 			writer = PdfFileWriter()
 
 			first_page = pdf.getPage(0)
 			last_page = pdf.getPage(pdf.getNumPages() - 1)
+			check_first_page_noreplace(first_page, prob_nums[temp_count])
+			temp_count += 1
+			"""
 			check_first_page(first_page, prob_nums[temp_count])
 			writer.addPage(first_page)
 			for page_number in range(1, pdf.getNumPages()):
 				writer.addPage(page_number)
-			with open(filename_base + "result.pdf", 'wb') as out_file:
+			with open('new ' + filename, 'wb') as out_file:
 				writer.write(out_file)
-			temp_count += 1
+			
+			"""
 		else:
-			print(filename + 'is missing... please check')
+			print(filename + ' is missing... please check')
 			temp_count += 1
 	
 
