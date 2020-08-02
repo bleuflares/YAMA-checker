9148d53ee52da5d345fecd09b3a326760bf517cf
Roy Park <roy@Roys-MacBook-Air.local>
2020-08-02 21:24:30 +0900

    almost finished... printing the input pdf shows crazy words...

--- checker.py
+++ checker.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*- 
 #YAMA FORMAT CHECKER by bleuflares
+
 import sys
 import os
 import argparse
+import csv
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
@@ -35,19 +38,23 @@ def replace_text(content, replacements = dict()):
 
 def replace_firstpage(content, title_format):
 	lines = content.splitlines()
-
 	result = ""
 	count = 0
 
 	for line in lines:
 		if count == 0:
-			line - title_format
+			print(line)
+			print(title_format)
+			if line != title_format:
+				print('title format is wrong! automatically converted... please check')
+			line = title_format
 		elif count == 1:
 			question = line.split()
 			question[0] = title_format.split()[1] + '.'
+			if line != ' '.join(question):
+				print('question number format is wrong! automatically converted... please check')
 			line - ' '.join(question)
 		result += line + "\n"
-	print(result)
 	return result
 
 def process_data(object, replacements):
@@ -62,63 +69,103 @@ def process_data(object, replacements):
 	else:
 		object.setData(encoded_data)
 
-def check(instring, outstring):
-	if instring != outstring:
-		return outstring
-	else:
-		return instring
-
-
-def check_filename(filename, title):
-	metas = filename.split('_')
-	metas[0] = check(metas[0], title)
-	return '_'.join(metas)
 
 def check_first_page(first_page, title_format):
-	data = object.getData()
-	decoded_data = data.decode('utf-8')
-
 	contents = first_page.getContents()
-	replace_firstpage(contents, title_format)
+	data = contents.getData()
+	decoded_data = data.decode('utf-8')
+	replaced_data = replace_firstpage(decoded_data, title_format)
 	encoded_data = replaced_data.encode('utf-8')
-	if object.decodedSelf is not None:
-		object.decodedSelf.setData(encoded_data)
+	if contents.decodedSelf is not None:
+		contents.decodedSelf.setData(encoded_data)
 	else:
-		object.setData(encoded_data)
-"""
-def check_last_page(last_page, last_page_format):
-"""
+		contents.setData(encoded_data)
 
 if __name__ == "__main__":
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-i", "--input", required=True, help="path to PDF document")
-	ap.add_argument("-t", "--title", required=True, help="path to PDF document")
+	ap.add_argument("-u", "--uid", required=True, help="path to PDF document")
 	args = vars(ap.parse_args())
 
 	in_file = args["input"].decode('utf-8')
+	uid = args["uid"].decode('utf-8')
 	print(in_file)
-	title = args["title"].decode('utf-8')
+
+	f = open(in_file, 'r')
+	reader = csv.reader(f)
+	prob_nums = []
+	filenames = []
+	name = ''
+	subject = ''
+	line_count = 0
+	line_metas = []
+	line_contents = []
+	for line in reader:
+		if line_count == 0:
+			subject = line[0].split()[0]
+			line_count += 1
+			print(subject)
+			continue
+
+		if line_count == 1:
+			line_metas = line
+			for i in range(len(line_metas)):
+				if line_metas[i] == '':
+					line_metas[i] = line_metas[i - 1]
+			line_count += 1
+			continue
+
+		uid_temp = line[0]
+		if uid_temp == uid:
+			line_contents = line
+			name = line_contents[1]
+			for i in range(2, len(line_contents)):
+				if line_contents[i] != '':
+					prob_nums.append((line_metas[i].decode('utf-8') + ' ' + line_contents[i].decode('utf-8')).encode('utf-8'))
+	for prob in prob_nums:
+		metas = prob.split(' ')
+		if len(metas[0].split('-')) > 1:
+			filename_base = [metas[0].split('-')[1], metas[0].split('-')[0], metas[1], str(uid), name]
+			filenames.append(subject[3:len(subject)] + ' ' + '_'.join(filename_base) + '.pdf')
+		else:
+			filename_base = [metas[0], metas[1], str(uid), name]
+			filenames.append(subject[3:len(subject)] + ' ' + '_'.join(filename_base) + '.pdf')
+
+	temp_count = 0
 	
+	for filename in filenames:
+		print(filename)
+		if os.path.isfile(filename):
+			pdf = PdfFileReader(filename)
+			writer = PdfFileWriter()
+
+			first_page = pdf.getPage(0)
+			last_page = pdf.getPage(pdf.getNumPages() - 1)
+			check_first_page(first_page, prob_nums[temp_count])
+			writer.addPage(first_page)
+			for page_number in range(1, pdf.getNumPages()):
+				writer.addPage(page_number)
+			with open(filename_base + "result.pdf", 'wb') as out_file:
+				writer.write(out_file)
+			temp_count += 1
+		else:
+			print(filename + 'is missing... please check')
+			temp_count += 1
+	
+
+	"""
 	filename_base = in_file.replace(os.path.splitext(in_file)[1], "")
-	
 	filename_base = check_filename(filename_base, title)
+	print(filename_base)
 	metas = filename_base.split('_')
 	if len(metas[0].split()) > 1:
 		title_format = metas[1] + '-' + metas[0].split()[1] + ' ' + metas[2]
 	else:
 		title_format = metas[1] + '-' + metas[0] + ' ' + metas[2]
-
+	"""
 	# Provide replacements list that you need here
-	replacements = { 'PDF': 'DOC'}
 
-	pdf = PdfFileReader(in_file)
-	writer = PdfFileWriter()
-
-	first_page = pdf.getPage(0)
-	last_page = pdf.getPage(pdf.getNumPages() - 1)
-
-	check_first_page(first_page, title_format)
-	check_last_page(last_page)
+	#check_last_page(last_page)
 	"""
 	for page_number in range(0, pdf.getNumPages()):
 
@@ -134,7 +181,11 @@ if __name__ == "__main__":
 
 		writer.addPage(page)
 	"""
+
+	"""
 	writer.addPage(first_page)
-	writer.addPage(last_page)
-	with open(filename_base + ".result.pdf", 'wb') as out_file:
-		writer.write(out_file)
+	print(pdf.getNumPages())
+	if pdf.getNumPages() > 1:
+		print("here")
+		writer.addPage(last_page)
+"""
